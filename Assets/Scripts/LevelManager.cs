6ab8bcb873a8eceaced1be8b46763b30058b084b
TonIveSat <tonny.sato@gmail.com>
2017-01-24 23:29:11 +0100

    Imported menu system

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("LoadLevel requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    internal void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+}
