03341aaa5f1204ec8adcbbcc2aadaaaa80bd484e
TonIveSat <tonny.sato@gmail.com>
2017-02-01 22:21:31 +0100

    Laser damage

---
+++ Assets/Prefabs/Entities/Enemy/DamageControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DamageControl : MonoBehaviour
+{
+    public int InitialHealth = 20;
+
+    private int currentHealth;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentHealth = InitialHealth;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var hitScript = collision.gameObject.GetComponent<Projectile>();
+        if (hitScript != null)
+        {
+            Hit(hitScript.Damage);
+            hitScript.Hit();
+        }
+    }
+
+    private void Hit(int Damage)
+    {
+        print("Enemy Hit");
+        currentHealth -= Damage;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
