f7eb520649b9b623b1343a1e7bf06acce47358a8
TonIveSat <tonny.sato@gmail.com>
2017-02-10 23:43:06 +0100

    Music

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,17 +5,45 @@ public class MusicPlayer : MonoBehaviour
 {
     static MusicPlayer instance;
 
+    public AudioClip StartClip;
+    public AudioClip GameClip;
+    public AudioClip EndClip;
+
+    private AudioSource musicAudioSource;
+
     void Awake()
     {
-        if (instance == null)
+        if (instance == null || instance == this)
         {
             instance = this;
             GameObject.DontDestroyOnLoad(gameObject);
+            musicAudioSource = GetComponent<AudioSource>();
+            musicAudioSource.clip = StartClip;
+            musicAudioSource.loop = true;
+            musicAudioSource.Play();
         }
         else
         {
             GameObject.Destroy(gameObject);
         }
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        Debug.Log("MusicPlayer level loaded: " + level);
+        musicAudioSource.Stop();
+
+        switch (level)
+        {
+            case 0: musicAudioSource.clip = StartClip; break;
+            case 1: musicAudioSource.clip = GameClip; break;
+            case 2: musicAudioSource.clip = EndClip; break;
+            default:
+                break;
+        }
+
+        musicAudioSource.loop = true;
+        musicAudioSource.Play();
     }
 
     void Start()
